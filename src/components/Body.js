32914cf43281c7c4212a256513e5072cfd11185e
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-06 20:30:32 +0530

    home route children added

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,14 +10,17 @@ const Body = () => {
       {
         path: '/',
         element: <Home />,
-      },
-      {
-        path: '/login',
-        element: <Login />
-      },
-      {
-        path: 'signup', 
-        element: <SignUp />
+        children: [
+          {
+            path: '/login',
+            element: <Login />
+          },
+          {
+            path: '/signup',
+            element: <SignUp />
+          },
+
+        ]
       },
       {
         path: '/browse',
