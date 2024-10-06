4bed944b5fbc3a04ba028e982c9781fcb7fde476
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-06 16:25:20 +0530

    app router updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Login from './Login';
 import Browse from './Browse';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Home';
+import SignUp from './SignUp';
 const Body = () => {
 
     const appRouter = createBrowserRouter([
       {
         path: '/',
-        element: <Login />,
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: 'signup', 
+        element: <SignUp />
       },
       {
         path: '/browse',
