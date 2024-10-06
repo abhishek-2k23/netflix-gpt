69e3ca004b2aede3fa64166821c53b8dd5be2f4c
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-06 20:32:06 +0530

    gradient and buttons on condition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from '../asset/logo.png'
-const Header = () => {
+const Header = ({login}) => {
   return (
     <div className='absolute w-10/12 left-[8.5%] justify-between lg:gap-x-80 md:gap-36 gap-16 flex  items-center bg-gradient-to-b from-black to-transparent pt-2'>
       {/* logo */}
       <img src={logo} alt='logo' className='w-56'/>
 
       {/* buttons  */}
+      {
+        !login &&
       <div className='z-10 text-white flex gap-5 '>
         {/* first button */}
         <div className='px-7 py-1 cursor-pointer rounded-md border border-gray-500 '>
@@ -18,6 +20,7 @@ const Header = () => {
           Sign In
         </div>
       </div>
+}
 
 
     </div>
