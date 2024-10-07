508f9db76864e9a109fe274140f8355cf10d7493
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-07 23:32:23 +0530

    signup form feature and useContext

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,51 +1,149 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect } from "react"
+import { validate } from "../utils/validate"
+import {  useLocation, useNavigate,  } from "react-router-dom"
+import { AppContext } from "../context/AppContext"
 
 const Login = () => {
-  const [rememberMe, setRememberMe] = useState(true);
-  function handleChange(e) {
-    setRememberMe(!e.target.checked);
-    console.log(rememberMe)
- }
-  const submitform = () => {
-    console.log("submit form from Login");
-  }
-  
-  return (
-    <div className="absolute z-10 top-[15%] w-[30%] left-[35%] space-y-4 flex justify-center items-center  text-white bg-[#0000009d]">
+  const navigate = useNavigate();
+  const {
+    email,password,name,
+    errorMessage,setErrorMessage,
+    rememberMe, setRememberMe,
+    isSignupForm, setIsSignUpForm,
+    signupEmail
+} = useContext(AppContext);
 
+  //check the pathname
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    console.log(signupEmail);
+    setIsSignUpForm(pathname === "/signup")
+    setErrorMessage(null);
+  }, [])
+
+  //for checkbox change
+  function handleChange(e) {
+    setRememberMe(!e.target.checked)
+    console.log(rememberMe)
+  }
+
+  //login form submission function
+  const submitLoginForm = () => {
+    //email and password validation
+    const message = validate(email.current.value, password.current.value)
+    setErrorMessage(message)
+  }
+
+  //signup form submission function
+  const submitSignupForm = () => {
+    //email and password validation
+    const message = validate(email.current.value, password.current.value)
+    console.log(email.current.value, name.current.value, password.current.value);
+    setErrorMessage(message)
+  }
+
+  //toggle the form
+  const toggle = () => {
+    if(isSignupForm){
+      navigate("/login");
+    }else{
+      navigate("/signup");
+    }
+    setIsSignUpForm(!isSignupForm);
+  }
+
+  return (
+    <div className="absolute z-10 top-[8%] w-[30%] left-[35%] space-y-4 flex justify-center items-center  text-white bg-[#0000009d] rounded-md">
       {/* content  */}
-      <div className='w-3/4 py-12 flex flex-col gap-4'>
+      <div className="w-3/4 py-12 flex flex-col gap-4">
         {/* heading  */}
-        <h1 className='text-4xl font-bold tracking-wide mb-8'>Sign In</h1>
+        <h1 className="text-4xl font-bold tracking-wide mb-8">{isSignupForm ? "Sign Up": "Sign In"}</h1>
 
         {/* form  */}
-        <form className='flex flex-col gap-5 text-white'>
-            <input type='email' placeholder='Email Address' className='flex-1 border border-gray-500 bg-transparent px-5 py-5 rounded' />
-            <input type='password' placeholder='Password' className='flex-1 border border-gray-500 bg-transparent px-5 py-5 rounded' />
+        <form
+          className="flex flex-col gap-5 text-white"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          {
+            isSignupForm && 
+            <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="flex-1 border border-gray-500 bg-transparent px-5 py-5 rounded"
+          />
+          }
+          <input
+            ref={email}
+            type="email"
+            value={signupEmail}
+            disabled = {isSignupForm && signupEmail!== null}
+            placeholder="Email Address"
+            className="flex-1 border border-gray-500 bg-transparent px-5 py-5 rounded"
+          />
+          <input
+            ref={password}
+            type="password"
+            placeholder="Password"
+            className="flex-1 border border-gray-500 bg-transparent px-5 py-5 rounded"
+          />
 
-            {/* signin button  */}
-            <div className='flex-1 bg-red-700 text-center rounded-md py-4 font-bold tracking-wide text-lg'>Sign In</div>
+          {/* Error message */}
+          {errorMessage != null && (
+            <p className="pl-5 text-red-500 font-semibold text-lg">
+              {errorMessage}
+            </p>
+          )}
+          {/* signin button  */}
+          <div
+            className="flex-1 bg-red-700 text-center rounded-md py-4 font-bold tracking-wide text-lg"
+            onClick={isSignupForm ? submitSignupForm : submitLoginForm}
+          >
+            {isSignupForm ? "SignUp" : "SignIn"}
+          </div>
         </form>
 
-
-        <p className='text-center text-lg'>OR</p>
+        <p className="text-center text-lg">OR</p>
 
         {/* signin using google  */}
-        <div className='flex-1 py-3 text-center text-white  bg-[#dbdbdb39] rounded-md'><span className='text-white opacity-100'>Sign In Using Google</span></div>
-
-        {/* forgot password  */}
-        <p className='text-center hover:text-gray-300 text-lg hover:cursor-pointer hover:underline'>Forgot password?</p>
-
-        {/* check box  */}
-        <div className='flex gap-3 items-center'>
-        <input value = "false" type = "checkbox" onChange = {handleChange} className='w-4 h-4'/>
-         <p className='text-lg'> Remember Me</p> 
+        <div className="flex-1 py-3 text-center text-white  bg-[#dbdbdb39] rounded-md">
+          <span className="text-white opacity-100">{isSignupForm ?"Sign up Using Google": "Sign In Using Google"}</span>
         </div>
 
-          {/* signup now  */}
-        <p className='text-lg'> New to Netflix? <span className='text-center hover:text-gray-300 text-lg hover:cursor-pointer hover:underline'>Sign up now</span></p>
+        {/* forgot password  */}
+       {!isSignupForm && <p className="text-center hover:text-gray-300 text-lg hover:cursor-pointer hover:underline">
+          Forgot password?
+        </p>}
 
-        <p className='text-sm mt-2'> This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className='text-center text-blue-700 hover:text-gray-300 text-lg hover:cursor-pointer hover:underline'>Learn more. </span> </p>
+        {/* check box  */}
+       {!isSignupForm && <div className="flex gap-3 items-center">
+          <input
+            value="false"
+            type="checkbox"
+            onChange={handleChange}
+            className="w-4 h-4"
+          />
+          <p className="text-lg"> Remember Me</p>
+        </div>}
+
+        {/* signup now  */}
+        <p className="text-lg" >
+          {" "}
+          {isSignupForm ? "Already a user? " : "New to Netflix? "}
+          
+            <span onClick={toggle} className="text-center hover:text-gray-300 text-lg hover:cursor-pointer hover:underline">
+              {isSignupForm ? "SignIn Now" : "SignUp Now"}
+            </span>
+        </p>
+
+        <p className="text-sm mt-2">
+          {" "}
+          This page is protected by Google reCAPTCHA to ensure you're not a bot.{" "}
+          <span className="text-center text-blue-700 hover:text-gray-300 text-lg hover:cursor-pointer hover:underline">
+            Learn more.{" "}
+          </span>{" "}
+        </p>
       </div>
     </div>
   )
