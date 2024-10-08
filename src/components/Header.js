f23b303cf4f182c3dc1350a7549432dd4b7d6795
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-09 00:54:13 +0530

    authState change code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,60 @@
 import React, { useState, useEffect } from "react"
 import logo from "../asset/logo.png"
 import { useLocation, useNavigate } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { signOut } from "firebase/auth"
+import { useSelector, useDispatch } from "react-redux"
+import { onAuthStateChanged, signOut } from "firebase/auth"
 import { auth } from "../utils/firebase"
 import { userLogoURL } from "../utils/url"
+import { addUser, removeUser } from "../redux/Slice/userSlice"
 
 const Header = () => {
   const { pathname } = useLocation()
   const [isBrowsePage, setIsBrowsePage] = useState(false)
-  const [isLogin, setIsLogin] = useState(false);
-
-  const user = useSelector(store => store.user);
-  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false)
+  const dispatch = useDispatch()
+  const user = useSelector((store) => store.user)
+  const navigate = useNavigate()
 
   const handleSignout = () => {
     signOut(auth).then(() => {
-      true && navigate("/login");
+      
     })
   }
 
   useEffect(() => {
-    
     if (pathname === "/browse") {
-      setIsBrowsePage(true);
+      setIsBrowsePage(true)
       console.log(isBrowsePage)
-      
     }
-      if (pathname === "/login") setIsLogin(true)
-    }, [])
+    if (pathname === "/login") setIsLogin(true)
+  }, [])
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user
+        dispatch(addUser({ uid, email, displayName, photoURL }))
+        navigate("/browse")
+      } else {
+        dispatch(removeUser())
+        navigate("/login")
+      }
+    })
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className={` ${isBrowsePage ? 'bg-transparent w-full px-36 items-center' : 'absolute w-10/12 left-[8.5%]  bg-gradient-to-b from-black to-transparent'} flex justify-between lg:gap-x-80 md:gap-36 gap-16   items-center pt-2`}>
+    <div
+      className={` ${
+        isBrowsePage
+          ? "bg-transparent w-full px-36 items-center"
+          : "absolute w-10/12 left-[8.5%]  bg-gradient-to-b from-black to-transparent"
+      } flex justify-between lg:gap-x-80 md:gap-36 gap-16   items-center pt-2`}
+    >
       {/* logo */}
       <img src={logo} alt="logo" className="w-56" />
 
       {/* buttons  only if signup page*/}
-      {(!isLogin && !isBrowsePage) && (
+      {!isLogin && !isBrowsePage && (
         <div className="z-10 text-white flex gap-5 ">
           {/* first button */}
           <div className="px-7 py-2 cursor-pointer rounded-md border border-gray-500 ">
@@ -44,28 +62,39 @@ const Header = () => {
           </div>
 
           {/* second button */}
-          <div onClick={() => navigate("/login")} className="px-7 py-2 cursor-pointer rounded-md  bg-red-700 font-bold tracking-wider">
+          <div
+            onClick={() => navigate("/login")}
+            className="px-7 py-2 cursor-pointer rounded-md  bg-red-700 font-bold tracking-wider"
+          >
             Sign In
           </div>
         </div>
       )}
 
       {/* buttons only if browse page  */}
-      {
-        isBrowsePage && (
-          <div className="z-10 text-white flex gap-5 ">
+      {isBrowsePage && (
+        <div className="z-10 text-white flex gap-5 ">
           {/* first button */}
           <div className="w-10 h-10 cursor-pointer rounded-md ">
-            <img src={(user !== null && user?.photoURL !== null) ? user?.photoURL : userLogoURL} alt="userlogo" />
+            <img
+              src={
+                user !== null && user?.photoURL !== null
+                  ? user?.photoURL
+                  : userLogoURL
+              }
+              alt="userlogo"
+            />
           </div>
 
           {/* second button */}
-          <div onClick={handleSignout} className="px-3 py-2 cursor-pointer rounded-md  bg-red-700 font-bold tracking-wide">
-            Sign out {user?.displayName?.split(' ')[0]}
+          <div
+            onClick={handleSignout}
+            className="px-3 py-2 cursor-pointer rounded-md  bg-red-700 font-bold tracking-wide"
+          >
+            Sign out {user?.displayName?.split(" ")[0]}
           </div>
         </div>
-        )
-      }
+      )}
     </div>
   )
 }
