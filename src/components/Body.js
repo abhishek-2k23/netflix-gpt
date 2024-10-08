29ac5670dd74797b181e6bf645d74bc7275059ac
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-08 18:58:59 +0530

    firebase authstate change trace

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Login from './Login';
 import Browse from './Browse';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Home';
 import SignUp from './SignUp'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../utils/firebase';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../redux/Slice/userSlice';
+import { AppContext } from '../context/AppContext';
+
+
 const Body = () => {
+    const dispatch = useDispatch();
+    const {setIsLoading} = useContext(AppContext);
 
     const appRouter = createBrowserRouter([
       {
@@ -31,6 +40,23 @@ const Body = () => {
         element: <Browse />
       }
     ])
+
+    //on user login/signup/signout
+    useEffect(() => {
+      
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const {uid, email, displayName, photoURL} = user;
+          dispatch(addUser({uid, email, displayName, photoURL }));
+          
+          // ...
+        } else {
+          // User is signed out
+          dispatch(removeUser());
+        }
+      });
+    }, [])
+
   return (
     <>
       <RouterProvider router={appRouter} />
