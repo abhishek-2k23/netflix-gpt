1aa53dee20c3bdd779c039de61792a1f949bbe45
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-11 19:26:45 +0530

    movie reducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./Slice/userSlice"
-
+import movieReducer from './Slice/moviesSlice'
 const store = configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
 })
 
