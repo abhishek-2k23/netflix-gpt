fa3f7f63e46ea590b640d4f65140142394823f1a
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-12 00:42:48 +0530

    movie related video api_url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,3 @@
 export const userLogoURL = "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.webp";
 export const nowPlayingMovieURL = 'https://api.themoviedb.org/3/movie/now_playing';
+export const movieVideoURL = 'https://api.themoviedb.org/3/movie/';
