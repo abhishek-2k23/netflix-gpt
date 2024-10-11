9fbc43643f8faad1261709bdd1f49e814ef1f8f3
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-12 00:43:21 +0530

    get the trailer key

---
+++ src/hooks/useGetTrailerKey.js
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import { movieVideoURL } from "../utils/url";
+import { API_OPTION } from "../utils/constants";
+import { addTrailerKey } from "../redux/Slice/moviesSlice";
+import { useEffect } from "react";
+
+const useGetTrailerKey = (movieID) => {
+    const dispatch = useDispatch();
+    const trailerKey = useSelector((store) => store?.movies?.trailerKey)
+  const getVideoClips = async () => {
+    const res = await fetch(
+      movieVideoURL + movieID + "/videos?language=en-US",
+      API_OPTION,
+    )
+    const videoData = await res.json()
+
+    //extracting only the trailers
+    let trailers = videoData?.results?.filter((v) => v.type === "Trailer")
+
+    //getting only one trailer if no trailer then take any video clip
+    let trailer = trailers.length ? trailers[0] : videoData[0]
+    dispatch(addTrailerKey(trailer?.key));
+  }
+
+  useEffect(() => {
+    getVideoClips()
+  }, [])
+
+  return trailerKey;
+}
+
+export default useGetTrailerKey;
