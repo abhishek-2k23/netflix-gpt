9360b2870b3946e8e7063bca0ae18f648c2f6676
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-11 19:29:01 +0530

    now playing movie url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,1 +1,2 @@
 export const userLogoURL = "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.webp";
+export const nowPlayingMovieURL = 'https://api.themoviedb.org/3/movie/now_playing';
