876b53bb03e2ebd9d1145de3b41e024571d7596c
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-13 22:57:02 +0530

    fetch topRatedMovies

---
+++ src/hooks/useGetTopRatedMovies.js
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux"
+import { topRatedMovieURL } from "../utils/url"
+import { API_OPTION } from "../utils/constants"
+import { addTopRatedMovies } from "../redux/Slice/moviesSlice"
+import { useEffect } from "react"
+
+const useGetTopRatedMovies = () => {
+  const dispatch = useDispatch()
+
+  //getNowPlayingMovies function
+  const getTopRatedMovies = async () => {
+    const res = await fetch(topRatedMovieURL, API_OPTION)
+    const movieData = await res.json()
+    dispatch(addTopRatedMovies(movieData?.results))
+  }
+
+  //call the function only once
+  useEffect(() => getTopRatedMovies, [])
+}
+
+export default useGetTopRatedMovies;
