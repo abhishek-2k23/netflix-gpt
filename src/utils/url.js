ace6d14ab27d1b47869aca7f5678529a53804d61
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-13 22:57:44 +0530

    toprated and popular movie api url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,6 @@
 export const userLogoURL = "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.webp";
 export const nowPlayingMovieURL = 'https://api.themoviedb.org/3/movie/now_playing';
+export const popularMovieURL = 'https://api.themoviedb.org/3/movie/popular';
+export const topRatedMovieURL = 'https://api.themoviedb.org/3/movie/top_rated';
 export const movieVideoURL = 'https://api.themoviedb.org/3/movie/';
+export const imageCDN = 'https://image.tmdb.org/t/p/w500'
