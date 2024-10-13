d497d601c4152676b74aff4d8ad015220be6f7b0
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-13 22:58:07 +0530

    toprated and popular movies custom hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,9 +3,13 @@ import Header from './Header'
 import useGetNowPlayingMovies from '../hooks/useGetNowPlayingMovies'
 import MainContainer from './browseComponents/MainContainer';
 import SecondaryContainer from './browseComponents/SecondaryContainer';
+import useGetPopularMovies from '../hooks/useGetPopularMovies';
+import useGetTopRatedMovies from '../hooks/useGetTopRatedMovies';
 
 const Browse = () => {
   useGetNowPlayingMovies();
+  useGetPopularMovies();
+  useGetTopRatedMovies();
   
   return (
     <div>
