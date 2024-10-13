785472b47a051a2f4213665c5619eba3f2d5d37b
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-13 22:59:15 +0530

    Made the movies list container

--- src/components/browseComponents/SecondaryContainer.js
+++ src/components/browseComponents/SecondaryContainer.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux';
 const SecondaryContainer = () => {
+
+  const {nowPlayingMovies, popularMovies, topRatedMovies} = useSelector((store) => store.movies);
+  
+  if(!nowPlayingMovies || !popularMovies || !topRatedMovies) return <div>Loading...</div>;
   return (
-    <div>SecondaryContainer</div>
+    <div className='bg-black'>
+      <div className='-mt-72'>
+      <MovieList title="Now Playing" movies={nowPlayingMovies}/>
+      <MovieList title="Popular Movies" movies={popularMovies}/>
+      <MovieList title="Top Rated Movies" movies={topRatedMovies}/>
+      <MovieList title="Upcoming Movies" movies={nowPlayingMovies}/>
+
+      </div>
+    </div>
   )
 }
 
