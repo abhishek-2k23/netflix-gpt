943db1d853798aa0b49c2f8481a38306830ef07f
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-15 19:55:53 +0530

    added env variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
+
 export const API_OPTION = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTk1MzZmZGNkNjRkN2M0OWUyMjZjNWE0ZmMzMjIyNiIsIm5iZiI6MTcyODY1Mjk1Mi45MjgxOTUsInN1YiI6IjY3MDkyNTVlYzVmY2Y1YTE2MzRmNzcwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J4Hf1vrG8VIGLDhlWZ8CfMaTEqZwsFND9i_wYymHLac'
+      Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_TOKEN}`
     }
   }; 
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDunSIOABM8fcJoPKaAj7zXabeijyIvH7g",
-  authDomain: "netflixgpt-abhishek01.firebaseapp.com",
-  projectId: "netflixgpt-abhishek01",
-  storageBucket: "netflixgpt-abhishek01.appspot.com",
-  messagingSenderId: "790034164633",
-  appId: "1:790034164633:web:a6b11c5f8f762bd91e4108",
-  measurementId: "G-J8CV29Z9SX"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
