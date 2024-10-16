3ee5a0454b17431fff69fa05c62b33920851b738
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-17 00:14:00 +0530

    implemeted gemini ai feature

--- src/components/browseComponents/GptSearch.js
+++ src/components/browseComponents/GptSearch.js
@@ -1,16 +1,42 @@
 import React, { useRef } from 'react'
 import bgImage from '../../asset/bgImage.jpg'
 import { FaSearch } from 'react-icons/fa'
+import { genAI } from '../../utils/geminiAI'
+import { useDispatch } from 'react-redux'
+import { setSearchResult } from '../../redux/Slice/gptSearch'
 const GptSearch = () => {
+    const dispatch = useDispatch(); 
+
+    //get the search input value
     const searchRef = useRef(null);
-    const handleSearch = () => {
-        console.log(searchRef.current.value);
+    
+    //initiate the gemini AI model
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    
+    const handleSearch = async () => {
+        try{
+            const prompt = "Act like a movie recommendation engine with the following prompt: " + searchRef.current.value + ". The output should be the name of 5 movie seperated with comma. For example: The Matrix, The Matrix Reloaded, The Matrix Revolutions, The Matrix Resurrections, The Matrix Revolutions. I only need the name of movie never the description. Only the name with comma seperation.";
+        
+            const result = await model.generateContent(prompt);
+            const response = result.response;
+            const text = response.text();
+
+            //convert movies name from text to array
+            const moviesNameArray = text.split(',');
+
+            //add to the redux store
+            dispatch(setSearchResult(moviesNameArray));
+        }catch(e){
+            console.log(e)
+        }
     }
+
   return (
     <div>
         {/* background  */}
         <img src={bgImage} alt="bgImage" className=''/>
-        <div className='absolute inset-0 bg-gradient-to-br from-[#000000ab] to-[#0000007a]'></div>
+        <div className='absolute inset-0 bg-gradient-to-br from-[#00000094] to-[#00000060]'></div>
 
         {/* search div  */}
         <div className='absolute top-[20%] left-1/4 w-1/2 bg-black rounded-md  p-5 '>
--- src/redux/Slice/gptSearch.js
+++ src/redux/Slice/gptSearch.js
@@ -4,12 +4,16 @@ const gptSearchSlice = createSlice({
     name: 'gptSearch',
     initialState: {
         isSearchPage: false,
+        searchResult: null,
     },
     reducers: {
         setIsSearchPage: (state, action) => {
             state.isSearchPage = action.payload;
+        }, 
+        setSearchResult: (state, action) => {
+            state.searchResult = action.payload;
         }
     }
 })
-export const {setIsSearchPage} = gptSearchSlice.actions;
+export const {setIsSearchPage, setSearchResult} = gptSearchSlice.actions;
 export default gptSearchSlice.reducer;
