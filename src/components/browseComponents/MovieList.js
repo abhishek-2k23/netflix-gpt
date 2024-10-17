6ce7f9cf1ce28e40ff9eac155aaa11dbe1c33d33
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-18 02:22:41 +0530

    css update

--- src/components/browseComponents/MovieList.js
+++ src/components/browseComponents/MovieList.js
@@ -3,11 +3,11 @@ import MovieCard from './MovieCard'
 const MovieList = ({title,movies}) => {
   
   return (
-    <div className='flex flex-col overflow-scroll no-scrollbar bg-black text-white p-1 '>
+    <div className='flex flex-col overflow-scroll no-scrollbar text-white p-1 '>
       <div className=' font-bold text-2xl pl-2 z-20'> {title}</div>
       <div className='flex '>
       {
-        movies.map((movie) => <div className=''><MovieCard key={movie.id} poster={movie.poster_path}/> </div>)
+        movies.map((movie) => <div className='' key={movie.id}><MovieCard  poster={movie.poster_path}/> </div>)
       }
       </div>
     </div>
