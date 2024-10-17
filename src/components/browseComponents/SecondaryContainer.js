e8705afa50645cec55a27aa6db32fd0211a0b50a
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-18 02:19:57 +0530

    showing only the movielist of available list

--- src/components/browseComponents/SecondaryContainer.js
+++ src/components/browseComponents/SecondaryContainer.js
@@ -5,14 +5,23 @@ const SecondaryContainer = () => {
 
   const {nowPlayingMovies, popularMovies, topRatedMovies} = useSelector((store) => store.movies);
   
-  if(!nowPlayingMovies || !popularMovies || !topRatedMovies) return <div>Loading...</div>;
+  if(!nowPlayingMovies) return <div>Loading...</div>;
   return (
     <div className='bg-black'>
       <div className='-mt-72'>
       <MovieList title="Now Playing" movies={nowPlayingMovies}/>
-      <MovieList title="Popular Movies" movies={popularMovies}/>
-      <MovieList title="Top Rated Movies" movies={topRatedMovies}/>
-      <MovieList title="Upcoming Movies" movies={nowPlayingMovies}/>
+      {
+        popularMovies && 
+        <MovieList title="Popular Movies" movies={popularMovies}/>
+      }
+      {
+        topRatedMovies && 
+        <MovieList title="Top Rated Movies" movies={topRatedMovies}/>
+      }
+      {
+        nowPlayingMovies &&
+        <MovieList title="Upcoming Movies" movies={nowPlayingMovies}/>
+      }
 
       </div>
     </div>
