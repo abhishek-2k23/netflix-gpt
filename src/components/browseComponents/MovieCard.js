2aa7a9be94376f62ca99f36dbb5e17d746819dfd
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-18 02:21:53 +0530

    avoid movies without poster

--- src/components/browseComponents/MovieCard.js
+++ src/components/browseComponents/MovieCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { imageCDN } from '../../utils/url'
 
 const MovieCard = ({poster}) => {
+  if(!poster) return null;
   return (
     <div className='w-48  cursor-pointer relative p-2 rounded-lg'>
       <img src={imageCDN+poster} alt="poster" className='w-full h-auto block rounded-lg'/>
