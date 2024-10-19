57010869118c816adecc463ce1084c3257b30ed7
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-19 23:51:33 +0530

    loader

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='w-full min-h-20 flex justify-center items-center'>
+        <div className='loader'></div>
+    </div>
+  )
+}
+
+export default Loader
