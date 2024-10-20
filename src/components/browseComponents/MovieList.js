a251d90acde109bc715c3ecf46e9aded7095d29c
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-20 23:46:39 +0530

    fixed title scroll issue

--- src/components/browseComponents/MovieList.js
+++ src/components/browseComponents/MovieList.js
@@ -3,9 +3,9 @@ import MovieCard from './MovieCard'
 const MovieList = ({title,movies}) => {
   
   return (
-    <div className='flex flex-col overflow-scroll no-scrollbar text-white p-1 '>
+    <div className='flex flex-col  md:overflow-auto  text-white p-1 '>
       <div className=' font-semibold md:font-bold text-lg md:text-2xl pl-2 z-20'> {title}</div>
-      <div className='flex '>
+      <div className='flex overflow-scroll no-scrollbar'>
       {
         movies.map((movie) => <div className='w-36 md:w-48' key={movie.id}><MovieCard  poster={movie.poster_path}/> </div>)
       }
