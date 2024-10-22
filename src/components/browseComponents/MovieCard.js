132aa052920eebe96afc260585d6a8c4aa7480b4
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-23 01:26:05 +0530

    removed link from here

--- src/components/browseComponents/MovieCard.js
+++ src/components/browseComponents/MovieCard.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import { imageCDN } from '../../utils/url'
-import { Link } from 'react-router-dom';
+import React from "react"
+import { imageCDN } from "../../utils/url"
+import { Link } from "react-router-dom"
 
-const MovieCard = ({id,poster}) => {
-  if(!poster) return null;
+const MovieCard = ({ id, poster }) => {
+  if (!poster) return null
   return (
-    <Link to={`/movie/${id}`}><div  className='w-32 md:w-48 cursor-pointer relative p-2 rounded-lg'>
-      <img src={imageCDN+poster} alt="poster" className='w-full h-auto block rounded-lg'/>
+    <div className="w-32 md:w-48 cursor-pointer relative p-2 rounded-lg">
+      <img
+        src={imageCDN + poster}
+        alt="poster"
+        className="w-full h-auto block rounded-lg"
+      />
     </div>
-    </Link>
   )
 }
 
