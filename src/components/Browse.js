6058a65c1f72633aa852da92c779e78b6401a947
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-23 01:14:46 +0530

    show movie info component

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -7,13 +7,23 @@ import useGetPopularMovies from '../hooks/useGetPopularMovies';
 import useGetTopRatedMovies from '../hooks/useGetTopRatedMovies';
 import { useSelector } from 'react-redux';
 import GptSearch from './browseComponents/gptSearch/GptSearch';
+import Movie from './MovieInfo/Movie';
 const Browse = () => {
   const isSearchPage = useSelector((store) => store.gptSearch.isSearchPage);
+  const {showMovieInfo} = useSelector((store) => store.movies);
+
   useGetNowPlayingMovies();
   useGetPopularMovies();
   useGetTopRatedMovies();
   return (
-    <div>
+    <div className={`${showMovieInfo && 'bg-fixed'}`}>
+
+      {
+        showMovieInfo && 
+          <div  className='fixed overflow-scroll z-50 w-screen h-screen  backdrop-blur-sm inset-0 bg-black/70 flex justify-center items-center'>
+            <Movie />
+          </div>
+      }
       <Header />
       
       {isSearchPage ? <GptSearch /> : (<><MainContainer /><SecondaryContainer /></>)}
