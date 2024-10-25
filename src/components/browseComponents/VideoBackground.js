e76be403d285aa3cd92ca1770dd842a7e751e860
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-25 22:28:52 +0530

    horizontal scroll bug

--- src/components/browseComponents/VideoBackground.js
+++ src/components/browseComponents/VideoBackground.js
@@ -6,10 +6,10 @@ const VideoBackground = ({ movieID }) => {
   const trailerKey = useGetTrailerKey(movieID);
 
   return (
-    <div className="w-screen">
-      <div className=" md:-mt-4">
+    <div className="w-full overflow-hidden">
+      <div className="-mt-4 md:-mt-8">
         <iframe
-          className=" w-screen  aspect-video"
+          className=" w-screen aspect-video "
           src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&controls=0&loop=1&mute=1`}
           title="YouTube video player"
           allowFullScreen
