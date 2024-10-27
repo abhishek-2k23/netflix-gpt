73ef4d46dec1ec653dd6ac6885859b02fcee1a2c
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-27 23:05:19 +0530

    center alignment of description

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,18 @@
-import React, { useContext, useEffect} from "react"
+import React, { useContext, useEffect } from "react"
 import { AppContext } from "../context/AppContext"
 import { useNavigate } from "react-router-dom"
-import {MdKeyboardArrowRight} from 'react-icons/md'
+import { MdKeyboardArrowRight } from "react-icons/md"
 
 const SignUp = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-
-  const {email, errorMessage, setErrorMessage, setSignUpForm,  setSignupEmail} = useContext(AppContext);
+  const {
+    email,
+    errorMessage,
+    setErrorMessage,
+    setSignUpForm,
+    setSignupEmail,
+  } = useContext(AppContext)
 
   useEffect(() => {
     return setErrorMessage(null)
@@ -29,49 +34,54 @@ const SignUp = () => {
     }
 
     setSignUpForm(true)
-    setSignupEmail(email.current.value);
+    setSignupEmail(email.current.value)
     navigate("/signup")
   }
 
-  
   return (
-    <div className="absolute z-10 top-1/4 md:w-[40%] md:left-[30%] md:space-y-4   text-center">
+    <div className="absolute w-screen h-screen flex flex-col text-center justify-center items-center z-10">
       {/* description  */}
-      <div>
+      <div className="">
         {/* text for the large screen  */}
-        <p className="md:block hidden text-7xl text-white font-bold mb-2">
-        Unlimited movies, TV shows and more
+        <p className="md:block hidden text-6xl text-white font-bold mb-3">
+          Unlimited movies,<br></br> TV shows and more
         </p>
-        <p className="md:hidden text-white font-bold mb-1 md:mb-0">
-          <div className="text-4xl md:text-7xl">Unlimited</div> <div className="text-4xl md:text-7xl">movies, TV </div> <div className="text-4xl md:text-7xl">shows and more</div> 
+        <p className="md:hidden text-white font-bold mb-1 md:mb-0 mx-auto">
+          <div className="text-4xl md:text-7xl">Unlimited</div>{" "}
+          <div className="text-4xl md:text-7xl">movies, TV </div>{" "}
+          <div className="text-4xl md:text-7xl">shows and more</div>
         </p>
-        <p className="text-lg md:text-xl text-white font-semibold ">
+        <p className="text-lg md:text-2xl text-white font-semibold ">
           Starts at ₹149. Cancel at any time.
         </p>
         <p className="text-lg md:text-xl text-white font-semibold pt-1 md:pt-10 px-2">
-          Ready to watch? Enter your email<br className="md:hidden"></br>  to create or restart your membership.
+          Ready to watch? Enter your email<br className="md:hidden"></br> to
+          create or restart your membership.
         </p>
       </div>
 
       {/* from */}
       <form
         onSubmit={(e) => e.preventDefault()}
-        className={`flex md:flex-row flex-col pt-5 md:pt-0 gap-3 text-white justify-between items-center`}
+        className={`w-3/4 sm:w-[38%] flex sm:flex-row flex-col pt-5 md:pt-2 gap-3 text-white justify-between items-center`}
       >
-      {/* email input field / */}
+        {/* email input field / */}
         <input
           ref={email}
           type="email"
           placeholder="Email Address"
-          className="flex-1 border border-gray-500 bg-transparent px-5 py-3 md:py-5 rounded"
+          className="w-full sm:flex-1 border border-gray-500 bg-transparent px-5 py-3 md:py-5 rounded"
         />
 
         {/* submit button  */}
         <button
-          className=" md:w-1/3 px-4 md:px-0 py-3 md:py-5 bg-red-700 rounded font-bold md:text-xl text-lg flex gap-2 items-center justify-center"
+          className="w-full sm:w-1/3 px-4 md:px-0 py-3 md:py-5 bg-red-700 rounded font-bold md:text-xl text-lg flex gap-2 items-center justify-center"
           onClick={handleFormSubmission}
         >
-          Get Started <span className="text-3xl"><MdKeyboardArrowRight /></span>
+          Get Started{" "}
+          <span className="text-3xl">
+            <MdKeyboardArrowRight />
+          </span>
         </button>
       </form>
 
