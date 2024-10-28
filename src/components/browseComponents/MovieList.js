9384e0c7895e5d8cd21f7e7be95decc7b319765a
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-10-28 23:54:38 +0530

    removed onclick from here

--- src/components/browseComponents/MovieList.js
+++ src/components/browseComponents/MovieList.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import MovieCard from './MovieCard'
-import useForOneMovie from '../../hooks/useForOneMovie'
-import useGetTrailerKey from '../../hooks/useGetTrailerKey';
-const MovieList = ({title,movies}) => {
-  const {handleMovieCard} = useForOneMovie();
-  const {getVideoClips} = useGetTrailerKey();
-
+import React from "react"
+import MovieCard from "./MovieCard"
+const MovieList = ({ title, movies }) => {
   return (
-    <div className='flex flex-col  md:overflow-auto  text-white p-1 '>
-      <div className=' font-semibold md:font-bold text-lg md:text-2xl pl-2 z-20'> {title}</div>
-      <div className='flex overflow-scroll no-scrollbar'>
-      {
-        movies.map((movie) => <div onClick={() => {handleMovieCard(movie.id); getVideoClips(movie.id, true)}} className='w-36 md:w-48' key={movie.id}><MovieCard  id={movie.id} poster={movie.poster_path}/> </div>)
-      }
+    <div className="flex flex-col  md:overflow-auto  text-white p-1 ">
+      <div className=" font-semibold md:font-bold text-lg md:text-2xl pl-2 z-20">
+        {" "}
+        {title}
+      </div>
+      <div className="flex overflow-scroll no-scrollbar">
+        {movies.map((movie) => (
+          <div className="w-36 md:w-48" key={movie.id}>
+            <MovieCard id={movie.id} poster={movie.poster_path} />{" "}
+          </div>
+        ))}
       </div>
     </div>
   )
